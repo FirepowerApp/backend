4013df10f18a28288f1a46ebea4219e4eae5531b
NelsonBlakeN <mail@blakenelson.me>
2025-06-17 07:56:58 -0500

    Force reschedule if execution end time is set and not reached

--- watchgameupdates/internal/services/rescheduler.go
+++ watchgameupdates/internal/services/rescheduler.go
@@ -10,13 +10,13 @@ func ShouldReschedule(payload models.Payload, lastPlay models.Play) bool {
 	if payload.ExecutionEnd != nil {
 		executionEnd, err := time.Parse(time.RFC3339, *payload.ExecutionEnd)
 		if err != nil {
-			// http.Error(w, "Invalid scheduled_time format", http.StatusBadRequest)
 			log.Printf("Error parsing executionEnd: %v", err)
 		} else if time.Now().After(executionEnd) {
-			log.Printf("Current time is after max execution time (%s). Do not reschedule.", executionEnd.Format(time.RFC3339))
+			log.Printf("Max execution time (%s) is set and has been reached. Do not reschedule.", executionEnd.Format(time.RFC3339))
 			return false
 		} else {
-			log.Printf("Current time is before max execution time (%s).", executionEnd.Format(time.RFC3339))
+			log.Printf("Max execution time (%s) is set and not reached. Continue exection.", executionEnd.Format(time.RFC3339))
+			return true
 		}
 	} else {
 		log.Println("Max execution time not set, proceeding without time check.")
